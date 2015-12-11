d75f26a532bd0f1be8a9853ff50adecd2f45f2bb
Amerrnath <amerrnath.21@gmail.com>
2015-12-11 12:12:51 +0530

    Seperation of variables to configurable

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,38 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var plugins = require('gulp-load-plugins')();
+
+var scss_src = ["app/scss/*.scss", "app/scss/*.sass"];
+var scss_dest = "app/css";
+var coffee_src = ["app/coffee"];
+var coffee_dest = "app/js";
+var index_file = "index.html";
+var jade_src = ["app/jade/**.jade"];
+var jade_dest= "app/";
+// ErrorHandler, gulp watch needs 'end' event to proceed to cycle without breaking watch flow.
 function errorHandler(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
-gulp.task('watch-css', function() {
-  return gulp.src('app/scss/*.scss')
+// Task to Compile scss to css
+gulp.task('sass2css', function() {
+  return gulp.src(scss_src)
     .pipe(plugins.plumber())
     .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest(scss_src))
     .pipe(plugins.connect.reload());
 });
 
-gulp.task('coffee', function(){
-  return gulp.src('app/coffee/*.coffee')
+// Task to Compile coffee to js
+gulp.task('coffee2js', function(){
+  return gulp.src(coffee_src)
     .pipe(plugins.coffee({bare: true}).on('error',errorHandler ))
-    .pipe(gulp.dest('app/js'))
+    .pipe(gulp.dest(coffee_dest))
     .pipe(plugins.connect.reload());
 });
 
+// Local static server
 gulp.task('webserver', function() {
   return plugins.connect.server({
     root: '.',
@@ -30,44 +42,15 @@ gulp.task('webserver', function() {
 
 });
 
+// Wire up third-part Dependencies library to HTML Markup
 gulp.task('bower', function () {
-  gulp.src('app/action-center.html')
+  gulp.src(index_file)
     .pipe(wiredep())
     .pipe(gulp.dest('.'));
 
 });
 
-gulp.task('jade2html', function (){
-  return gulp.src('app/jade/**.jade')
-    .pipe(plugins.jade({pretty: true}).on('error', errorHandler ))
-    .pipe(gulp.dest('app/'))
-    .pipe(plugins.connect.reload());
-});
-
-
-gulp.task('w', function (){
-  gulp.watch(['./app/sass/*.scss'], ['watch-css']);
-  gulp.watch(['./app/coffee/**.coffee'], ['coffee']);
-  gulp.watch(['app/**/**.html'], function (event){
-      return gulp.src(event.path).pipe(plugins.connect.reload());
-  });
-});
-
-gulp.task('watch', function (){
-  gulp.watch(['app/scss/**.scss'], ['watch-css']);
-  //
-  gulp.watch(['app/js/**.js'], function (event){
-      return gulp.src(event.path).pipe(plugins.connect.reload());
-  });
-  gulp.watch(['app/jade/**.jade'], ['jade2html']);
-
-  gulp.watch(['app/jade/layouts/**.jade', 'app/jade/views/**.jade'], ['jade2html']);
-
-  // gulp.watch(['**/**.html'], function (event){
-  //     return gulp.src(event.path).pipe(plugins.connect.reload());
-  // });
-});
-
+// Wire up third-part Dependencies library to Jade Markup
 gulp.task('wire-jade', function (){
   gulp.src('app/jade/layouts/*.jade')
     .pipe(wiredep({
@@ -76,6 +59,34 @@ gulp.task('wire-jade', function (){
     }))
     .pipe(gulp.dest('app/jade/layouts/'));
 });
+
+// Taskt to Convert Jade to hTML
+gulp.task('jade2html', function (){
+  return gulp.src(jade_src)
+    .pipe(plugins.jade({pretty: true}).on('error', errorHandler ))
+    .pipe(gulp.dest(jade_dest))
+    .pipe(plugins.connect.reload());
+});
+
+
+gulp.task('watch', function (){
+  gulp.watch(sass_src, ['sass2css']);
+
+  gulp.watch(coffee_src, ['coffee2js']);
+
+  gulp.watch(['app/js/**.js'], function (event){
+      return gulp.src(event.path).pipe(plugins.connect.reload());
+  });
+
+  gulp.watch(jade_src, ['jade2html']);
+
+  // If there is any paritals in Jade
+  gulp.watch(['app/jade/layouts/**.jade', 'app/jade/views/**.jade'], ['jade2html']);
+
+});
+
+
+
 
 gulp.task('build', function (){
 
--- Readme.md
+++ Readme.md
@@ -1,4 +1,12 @@
 ### Frontend Automation scripts
 
-I am trying to publish some of my default  automation task involved in Frontend.
+I am trying to publish some of my default automation task involved in Frontend.
 I hope to update scripts to make more generic and customizable.
+
+This repo is will be used as boilerplate for frontend productivity tools.
+Saving time to write gulp automation task for each cases.
+
+### [Gulp] is to Automate and enhance your workflow.
+
+
+[Gulp]: <http://gulpjs.com>
